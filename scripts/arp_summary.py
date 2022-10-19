d210e431b76a84b862f04ada49cf3456df8783b4
Allen Robel <arobel@cisco.com>
2022-10-19 13:42:30 -1000

    Replace Thread with ThreadPoolExecutor

--- scripts/arp_summary.py
+++ scripts/arp_summary.py
@@ -9,7 +9,7 @@ script_name = 'arp_summary'
 
 # standard libraries
 import argparse
-from threading import Thread, Lock
+from concurrent.futures import ThreadPoolExecutor
 # local libraries
 from args.args_cookie import ArgsCookie
 from args.args_nxapi_tools import ArgsNxapiTools
@@ -47,6 +47,13 @@ def print_header():
         'throttled',
         'unknown',
         'total'))
+def print_output(futures):
+    for future in futures:
+        output = future.result()
+        if output == None:
+            continue
+        for line in output:
+            print(line)
 
 def worker(device, vault):
     ip = get_device_mgmt_ip(nb, device)
@@ -54,17 +61,17 @@ def worker(device, vault):
     arp.nxapi_init(cfg)
     arp.vrf = cfg.vrf
     arp.refresh()
-    with lock:
-        print("{:<15} {:<14} {:>10} {:>13} {:>15} {:>15} {:>12} {:>12}".format(
-            ip,
-            arp.hostname,
-            arp.vrf,
-            arp.resolved,
-            arp.incomplete,
-            arp.throttled,
-            arp.unknown,
-            arp.total))
-
+    lines = list()
+    lines.append(fmt.format(
+        ip,
+        arp.hostname,
+        arp.vrf,
+        arp.resolved,
+        arp.incomplete,
+        arp.throttled,
+        arp.unknown,
+        arp.total))
+    return lines
 cfg = get_parser()
 log = get_logger(script_name, cfg.loglevel, 'DEBUG')
 vault = get_vault(cfg.vault)
@@ -76,7 +83,9 @@ devices = get_device_list()
 fmt = '{:<15} {:<14} {:>10} {:>13} {:>15} {:>15} {:>12} {:>12}'
 print_header()
 
-lock = Lock()
+executor = ThreadPoolExecutor(max_workers=len(devices))
+futures = list()
 for device in devices:
-    t = Thread(target=worker, args=(device, vault))
-    t.start()
+    args = [device, vault]
+    futures.append(executor.submit(worker, *args))
+print_output(futures)
