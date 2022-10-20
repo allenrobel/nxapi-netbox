b6e4ce876abdf58962afe9bdf3d0716ad5a14f95
Allen Robel <arobel@cisco.com>
2022-10-20 09:50:13 -1000

    Replace Thread with ThreadPoolExecutor

--- scripts/bgp_neighbors.py
+++ scripts/bgp_neighbors.py
@@ -8,14 +8,12 @@ Synopsis:
 ./bgp_neighbors_format_table.py --vault hashicorp --devices cvd_leaf_1
 '''
 
-our_version = 104
+our_version = 105
 script_name = 'bgp_neighbors_format_table'
 
 # standard libraries
 import argparse
-from threading import Thread, Lock
-
-# local libraries
+from concurrent.futures import ThreadPoolExecutor
 # local libraries
 from args.args_cookie import ArgsCookie
 from args.args_nxapi_tools import ArgsNxapiTools
@@ -44,8 +42,15 @@ def get_device_list():
         log.error('exiting. Cannot parse --devices {}.  Example usage: --devices leaf_1,spine_2,leaf_2'.format(cfg.devices))
         exit(1)
 
+def print_output(futures):
+    for future in futures:
+        output = future.result()
+        if output == None:
+            continue
+        for line in output:
+            print(line)
+
 def get_max_key_length(d):
-    #log.info('d {}'.format(d))
     length = 0
     for peer in d:
         for key in d[peer]:
@@ -58,37 +63,44 @@ def get_max_key_length(d):
                     length = len(str(k))
     return length
 
-def print_header(d):
+def get_header(d):
     key_length = get_max_key_length(d)
-    print('{:<15} {:<15} {:<{length}} {}'.format('dut', 'peer', 'key', 'value', length=key_length))
+    return '{:<15} {:<15} {:<{length}} {}'.format('dut', 'peer', 'key', 'value', length=key_length)
 
-def print_dict(d, hostname):
+def collect_output(d, hostname):
     key_length = get_max_key_length(d)
+    lines = list()
     for peer in d:
         for key in sorted(d[peer]):
             value = d[peer][key]
             if type(value) != type(dict()):
-                print("{:<15} {:<15} {:<{length}} {}".format(hostname, peer, key, value, length=key_length))
+                lines.append("{:<15} {:<15} {:<{length}} {}".format(hostname, peer, key, value, length=key_length))
                 continue
             for k in value:
-                print("{:<15} {:<15} {:<{length}} {}".format(hostname, peer, k, value[k], length=key_length))
-        print()
+                lines.append("{:<15} {:<15} {:<{length}} {}".format(hostname, peer, k, value[k], length=key_length))
+        lines.append('')
+    return lines
 
 def worker(device, vault):
     ip = get_device_mgmt_ip(nb, device)
     bgp = NxapiBgpNeighborsIpv4(vault.nxos_username, vault.nxos_password, ip, log)
     bgp.nxapi_init(cfg)
     bgp.refresh()
-    with lock:
-        print()
-        print_header(bgp.peer_global)
-        print_dict(bgp.peer_global, bgp.hostname)
+    lines = list()
+    lines.append('')
 
-        print_header(bgp.capextendednhaf)
-        print_dict(bgp.capextendednhaf, bgp.hostname)
+    lines.append(get_header(bgp.peer_global))
+    for line in collect_output(bgp.peer_global, bgp.hostname):
+        lines.append(line)
 
-        print_header(bgp.capextendednhsaf)
-        print_dict(bgp.capextendednhsaf, bgp.hostname)
+    lines.append(get_header(bgp.capextendednhaf))
+    for line in collect_output(bgp.capextendednhaf, bgp.hostname):
+        lines.append(line)
+
+    lines.append(get_header(bgp.capextendednhsaf))
+    for line in collect_output(bgp.capextendednhsaf, bgp.hostname):
+        lines.append(line)
+    return lines
 
 cfg = get_parser()
 log = get_logger(script_name, cfg.loglevel, 'DEBUG')
@@ -96,8 +108,11 @@ vault = get_vault(cfg.vault)
 vault.fetch_data()
 nb = netbox(vault)
 
-lock = Lock()
 devices = get_device_list()
+
+executor = ThreadPoolExecutor(max_workers=len(devices))
+futures = list()
 for device in devices:
-    t = Thread(target=worker, args=(device, vault))
-    t.start()
+    args = [device, vault]
+    futures.append(executor.submit(worker, *args))
+print_output(futures)
