e722c5bf72315bdc5c0a75f08e220d11f7b193a8
Allen Robel <arobel@cisco.com>
2022-10-21 11:24:06 -1000

    Replace Thread with ThreadPoolExecutor

--- scripts/switch_version.py
+++ scripts/switch_version.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-our_version = 108
+our_version = 109
 script_name = 'switch_version'
 '''
 Name: switch_version.py
@@ -11,7 +11,7 @@ Example usage:
 '''
 # standard libraries
 import argparse
-from threading import Thread, Lock
+from concurrent.futures import ThreadPoolExecutor
 from time import sleep
 # local libraries
 from args.args_cookie import ArgsCookie
@@ -41,22 +41,32 @@ def get_device_list():
         log.error('exiting. Cannot parse --devices {}.  Example usage: --devices leaf_1,spine_2,leaf_2'.format(cfg.devices))
         exit(1)
 
+def print_output(futures):
+    for future in futures:
+        output = future.result()
+        if output == None:
+            continue
+        for line in output:
+            print(line)
+
 def print_header():
     print(fmt.format('ip', 'hostname', 'bios', 'nxos_version'))
 
-def print_info(ip, output):
-    print(fmt.format(
+def get_output(ip, nx):
+    lines = list()
+    lines.append(fmt.format(
         ip,
-        output[ip].hostname,
-        output[ip].bios_ver_str,
-        output[ip].nxos_ver_str))
+        nx.hostname,
+        nx.bios_ver_str,
+        nx.nxos_ver_str))
+    return lines
 
 def worker(device, vault):
     ip = get_device_mgmt_ip(nb, device)
     nx = NxapiVersion(vault.nxos_username, vault.nxos_password, ip, log)
     nx.nxapi_init(cfg)
     nx.refresh()
-    output[ip] = nx
+    return get_output(ip, nx)
 
 cfg = get_parser()
 log = get_logger(script_name, cfg.loglevel, 'DEBUG')
@@ -66,15 +76,12 @@ nb = netbox(vault)
 
 devices = get_device_list()
 
-output = dict()
-
 fmt = '{:<15} {:<20} {:<9} {:<32}'
 print_header()
 
-lock = Lock()
+executor = ThreadPoolExecutor(max_workers=len(devices))
+futures = list()
 for device in devices:
-    t = Thread(target=worker, args=(device, vault))
-    t.start()
-    t.join()
-for ip in sorted(output):
-    print_info(ip, output)
+    args = [device, vault]
+    futures.append(executor.submit(worker, *args))
+print_output(futures)
