da1441bb678d8cbde5734e37c7ba9d704f9e44d0
Allen Robel <arobel@cisco.com>
2022-10-20 14:30:10 -1000

    Replace Thread with ThreadPoolExecutor, more...
    Also, add example output

--- scripts/inventory_find_serial_numbers.py
+++ scripts/inventory_find_serial_numbers.py
@@ -2,13 +2,25 @@
 '''
 Name: inventory_find_serial_numbers.py
 Description: NXAPI: find one or more serial numbers across a set of NXOS switches
+
+Example output:
+
+% ./inventory_find_serial_numbers.py --vault hashicorp --devices cvd_leaf_1,cvd_leaf_2,cvd_l2_fanout,cvd_bgw_1 --serials FOC21055QF1,SAL1230Q3F5
+ip              hostname             serial       name            product_id       description                   
+192.168.11.116  cvd_l2_911           SAL1230Q3F5  Chassis         N3K-C3232C       Nexus3000 C3232C Chassis      
+192.168.11.116  cvd_l2_911           SAL1230Q3F5  Slot 1          N3K-C3232C       32x40/100G QSFP28 2x10G SFP+ Ethernet Module
+
+192.168.11.110  cvd-1111-bgw         FOC21055QF1  Slot 24         N9K-C9504-FM-E   4-slot Fabric Module          
+
+% 
+
 '''
-our_version = 110
+our_version = 111
 script_name = 'inventory_find_serial_numbers'
 
 #standard libraries
 import argparse
-from threading import Thread, Lock
+from concurrent.futures import ThreadPoolExecutor
 # local libraries
 from args.args_cookie import ArgsCookie
 from args.args_nxapi_tools import ArgsNxapiTools
@@ -21,7 +33,7 @@ from nxapi.nxapi_inventory import NxapiInventory
 def get_parser():
     help_serials = 'a comma-separated list (no spaces) of Nexus serial numbers'
     ex_prefix = 'Example:'
-    ex_serials = '{} --serials SAL2232U92Z,FOC321617XA,FOC631317XA'.format(ex_prefix)
+    ex_serials = '{} --serials SAL2222U92Z,FOC321017XA,FOC543217XA'.format(ex_prefix)
 
     parser = argparse.ArgumentParser(
         description='DESCRIPTION: NXAPI: find one or more serial numbers across a set of NXOS switches.',
@@ -54,6 +66,16 @@ def get_serial_number_list():
         log.error('exiting. Cannot parse --serials {}.  Example usage: --serials SAL2232U92Z,FOC321617XA,FOC631317XA'.format(cfg.serials))
         exit(1)
 
+def print_output(futures):
+    for future in futures:
+        output = future.result()
+        if output == None:
+            continue
+        for line in output:
+            print(line)
+        if len(output) > 0:
+            print()
+
 def print_header():
     print(fmt.format(
         'ip',
@@ -63,24 +85,27 @@ def print_header():
         'product_id',
         'description'))
 
+def collect_output(ip, nx):
+    lines = list()
+    for item in nx.info:
+        nx.item = item
+        if nx.serialnum not in serial_numbers:
+            continue
+        lines.append(fmt.format(
+            ip,
+            nx.hostname,
+            nx.serialnum,
+            nx.name,
+            nx.productid,
+            nx.desc))
+    return lines
+
 def worker(device, vault):
     ip = get_device_mgmt_ip(nb, device)
-    i = NxapiInventory(vault.nxos_username, vault.nxos_password, ip, log)
-    i.nxapi_init(cfg)
-    i.refresh()
-    d = i.info
-    with lock:
-        for item in d:
-            i.item = item
-            if i.serialnum not in serial_numbers:
-                continue
-            print(fmt.format(
-                ip,
-                i.hostname,
-                i.serialnum,
-                i.name,
-                i.productid,
-                i.desc))
+    nx = NxapiInventory(vault.nxos_username, vault.nxos_password, ip, log)
+    nx.nxapi_init(cfg)
+    nx.refresh()
+    return collect_output(ip, nx)
 
 cfg = get_parser()
 log = get_logger(script_name, cfg.loglevel, 'DEBUG')
@@ -94,8 +119,9 @@ serial_numbers = get_serial_number_list()
 fmt = '{:<15} {:<20} {:<12} {:<15} {:<16} {:<30}'
 print_header()
 
-lock = Lock()
+executor = ThreadPoolExecutor(max_workers=len(devices))
+futures = list()
 for device in devices:
-    t = Thread(target=worker, args=(device, vault))
-    t.start()
-
+    args = [device, vault]
+    futures.append(executor.submit(worker, *args))
+print_output(futures)
