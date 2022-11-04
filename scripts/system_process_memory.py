60a42d4a2a868a056920f00afbf84427ebf1e498
Allen Robel <arobel@cisco.com>
2022-11-03 16:06:41 -1000

    add --processname argument

--- scripts/system_process_memory.py
+++ scripts/system_process_memory.py
@@ -33,6 +33,16 @@ ip              hostname           process_name              virtual  physical r
 192.168.11.102  cvd-1311-leaf      adbm                      403108   10711    52284    1         25527
 192.168.11.102  cvd-1311-leaf      agetty                    42940    1446     8636     5         24767,24770,24771,24774,24776
 etc...
+
+Comparing process memory usage for one process across multiple switches:
+
+% ./system_process_memory.py --vault hashicorp --devices cvd_leaf_1,cvd_leaf_2,cvd_leaf_3,cvd_leaf_4 --processname vsh.bin --summed
+ip              hostname           process_name              virtual  physical rss      instances processid(s)
+192.168.11.102  cvd-1311-leaf      vsh.bin                   8001232  164588   954144   12        12780,13510,13663,14907,24368,24369,24371,24376,26701,26727,26739,27047
+192.168.11.103  cvd-1312-leaf      vsh.bin                   5624604  132032   780888   8         15130,15131,15133,15136,25563,26476,26563,27963
+192.168.11.104  cvd-1313-leaf      vsh.bin                   5496848  128874   729872   8         8856,8857,8859,8861,16761,17548,18574,19092
+192.168.11.105  cvd-1314-leaf      vsh.bin                   6190788  141882   825896   9         2115,2116,2118,2120,14086,14870,15700,15740,16401
+ % 
 '''
 # standard libraries
 import argparse
@@ -50,19 +60,29 @@ script_name = 'system_process_memory'
 
 def get_parser():
     help_summed = 'By default, processes are individually listed, by processid.  If --summed is present, then memory usage for processes with the same processname are summed and entry for the process name is printed, along with a list of processids for that name.'
+    help_processname = 'If present, retrieve stats only for this process.'
+
     ex_summed = 'Example: --summed'
+    ex_processname = 'Example: --processname dcos_sshd'
+
     parser = argparse.ArgumentParser(
         description='DESCRIPTION: NXAPI: display memory usage of system processes',
         parents=[ArgsCookie, ArgsNxapiTools])
     default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
     mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
 
-    mandatory.add_argument('--summed',
+    default.add_argument('--summed',
                         dest='summed',
                         required=False,
                         action='store_true',
                         default=False,
                         help='{} {} {}'.format('DEFAULT: %(default)s.', help_summed, ex_summed))
+
+    default.add_argument('--processname',
+                        dest='processname',
+                        required=False,
+                        default=None,
+                        help='{} {} {}'.format('DEFAULT: %(default)s.', help_processname, ex_processname))
 
     parser.add_argument('--version',
                         action='version',
@@ -96,12 +116,21 @@ def worker_by_processid(device, vault):
     nx.nxapi_init(cfg)
     nx.refresh()
     lines = list()
-    for process_id in nx.info_by_processid:
-        processname = nx.info_by_processid[process_id]['processname']
-        physical = nx.info_by_processid[process_id]['physical']
-        rss = nx.info_by_processid[process_id]['rss']
-        virtual = nx.info_by_processid[process_id]['virtual']
-        lines.append(fmt_worker_by_processid.format(ip, nx.hostname, processname, virtual, physical, rss, process_id))
+    if cfg.processname != None:
+        for process_id in nx.info_by_processid:
+            processname = nx.info_by_processid[process_id]['processname']
+            if processname == cfg.processname:
+                physical = nx.info_by_processid[process_id]['physical']
+                rss = nx.info_by_processid[process_id]['rss']
+                virtual = nx.info_by_processid[process_id]['virtual']
+                lines.append(fmt_worker_by_processid.format(ip, nx.hostname, processname, virtual, physical, rss, process_id))
+    else:
+        for process_id in nx.info_by_processid:
+            processname = nx.info_by_processid[process_id]['processname']
+            physical = nx.info_by_processid[process_id]['physical']
+            rss = nx.info_by_processid[process_id]['rss']
+            virtual = nx.info_by_processid[process_id]['virtual']
+            lines.append(fmt_worker_by_processid.format(ip, nx.hostname, processname, virtual, physical, rss, process_id))
     return lines
 
 # Use this to view summed memory stats for processes with the same name
@@ -111,9 +140,13 @@ def worker(device, vault):
     nx.nxapi_init(cfg)
     nx.refresh()
     lines = list()
-    for process in nx.process_names:
-        nx.process = process
+    if cfg.processname != None:
+        nx.process = cfg.processname
         lines.append(fmt_worker.format(ip, nx.hostname, nx.processname, nx.virtual, nx.physical, nx.rss, nx.instances, nx.processid))
+    else:
+        for process in nx.process_names:
+            nx.process = process
+            lines.append(fmt_worker.format(ip, nx.hostname, nx.processname, nx.virtual, nx.physical, nx.rss, nx.instances, nx.processid))
     return lines
 
 cfg = get_parser()
