ee3a7c8aced289c06953ca2400c6d1ef909447a1
Allen Robel <arobel@cisco.com>
2022-11-04 14:24:37 -1000

    various minor changes
    Move parser to def.
    Fix Column header labels.
    Pass nx object to collect_output() rather than individual items.
    Minor logic simplification.

--- scripts/interface_beacon_status.py
+++ scripts/interface_beacon_status.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python3
-our_version = 110
+our_version = 111
 '''
 Name: interface_beacon_status.py
 Description: NXAPI: display interface beacon status
 
-Example usage:
+Example output:
 
-./interface_beacon_status.py --vault hashicorp --devices leaf_1,leaf_2 [--interface Eth1/1] [--on]
+Using --interface:
+
+% ./interface_beacon_status.py --vault hashicorp --devices cvd_leaf_1 --interface eth1/49
+ip              hostname             interface            state   admin   beacon 
+192.168.11.102  cvd-1311-leaf        Ethernet1/49         up      up      on     
+% 
+
+Without --interface:
+
+% ./interface_beacon_status.py --vault hashicorp --devices cvd_leaf_1                    
+ip              hostname             interface            state   admin   beacon 
+192.168.11.102  cvd-1311-leaf        mgmt0                up      up      na     
+192.168.11.102  cvd-1311-leaf        Ethernet1/1          down    down    off    
+192.168.11.102  cvd-1311-leaf        Ethernet1/2          down    up      off
+etc...
+%
 
 NOTES:
 
-   1. mgmt0 is non-virtual, and currently does not support beacon.  We process it anyway in case it ever 
-      does support beacon
+1. mgmt0 is non-virtual, and currently does not support beacon.  We process it anyway in case it ever does support beacon
 '''
 script_name = 'interface_beacon_status'
 
@@ -27,18 +41,19 @@ from nxapi_netbox.vault.vault import get_vault
 from nxapi_netbox.nxapi.nxapi_interface import NxapiInterface
 from nxapi_netbox.nxapi.nxapi_interface import NxapiInterfaceStatus
 
-help_interface = 'If present, interface to monitor.  If not present, all interfaces will be monitored.'
-help_on = 'If present, print only interfaces whose beacon status is on. If not present, print all interface beacon status.'
-
-ex_interface = 'Example: --interface Eth1/1'
-ex_on = 'Example: --on'
-
 def get_parser():
+    help_interface = 'If present, interface to monitor.  If not present, all interfaces will be monitored.'
+    help_on = 'If present, print only interfaces whose beacon status is on. If not present, print all interface beacon status.'
+
+    ex_interface = 'Example: --interface Eth1/1'
+    ex_on = 'Example: --on'
+
     parser = argparse.ArgumentParser(
-        description='DESCRIPTION: NXAPI: display interface beacon status.',
+        description='DESCRIPTION: NXAPI: display interface beacon status',
         parents=[ArgsCookie, ArgsNxapiTools])
-    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
+
     mandatory = parser.add_argument_group(title='MANDATORY SCRIPT ARGS')
+    default   = parser.add_argument_group(title='DEFAULT SCRIPT ARGS')
 
     default.add_argument('--interface',
                         dest='interface',
@@ -54,7 +69,7 @@ def get_parser():
                         help='{} {}'.format(help_on, ex_on))
 
     parser.add_argument('--version',
-                        action='version',
+                        action='version', 
                         version='{} v{}'.format('%(prog)s', our_version))
     return parser.parse_args()
 
@@ -74,30 +89,19 @@ def print_output(futures):
             print(line)
 
 def print_header():
-    print(fmt.format('ip', 'hostname', 'interface', 'state', 'admin', 'beacon', 'dut'))
+    print(fmt.format('ip', 'hostname', 'interface', 'state', 'admin', 'beacon'))
 
-def get_values(ip, hostname, interface, state, admin, beacon):
-    if cfg.on == True and (beacon == 'off' or beacon == 'na'):
-        return None
-    return fmt.format(ip, hostname, interface, state, admin, beacon)
-
-def collect_output(ip, interface_status, interface_detail):
+def collect_output(ip, nx):
     lines = list()
-    for interface in interface_status.info:
-        if interface_detail.is_virtual_interface(interface):
-            continue
-        interface_detail.interface = interface
-        interface_detail.refresh()
-        x = get_values(ip,
-            interface_detail.hostname,
-            interface_detail.interface,
-            interface_detail.state,
-            interface_detail.admin_state,
-            interface_detail.eth_beacon)
-        if x != None:
-            lines.append(x)
-    if len(lines) != 0:
-        lines.append('')
+    if cfg.on == True and (nx.eth_beacon == 'off' or nx.eth_beacon == 'na'):
+        return lines
+    lines.append(fmt.format(
+        ip,
+        nx.hostname,
+        nx.interface,
+        nx.state,
+        nx.admin_state,
+        nx.eth_beacon))
     return lines
 
 def worker(device, vault):
@@ -108,12 +112,19 @@ def worker(device, vault):
     s.interface = cfg.interface
     s.refresh()
 
-    i = NxapiInterface(vault.nxos_username, vault.nxos_password, ip, log)
-    i.nxapi_init(cfg)
-    return collect_output(ip, s, i)
+    nx = NxapiInterface(vault.nxos_username, vault.nxos_password, ip, log)
+    nx.nxapi_init(cfg)
 
+    lines = list()
+    for interface in s.info:
+        if nx.is_virtual_interface(interface):
+            continue
+        nx.interface = interface
+        nx.refresh()
+        lines += collect_output(ip, nx)
+    lines.append('--')
+    return lines
 
-fmt = '{:<15} {:<18} {:<15} {:<7} {:<7} {:<6}'
 cfg = get_parser()
 log = get_logger(script_name, cfg.loglevel, 'DEBUG')
 vault = get_vault(cfg.vault)
@@ -122,6 +133,7 @@ nb = netbox(vault)
 
 devices = get_device_list()
 
+fmt = "{:<15} {:<20} {:<20} {:<7} {:<7} {:<7}"
 print_header()
 
 executor = ThreadPoolExecutor(max_workers=len(devices))
